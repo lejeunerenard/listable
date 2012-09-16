180e5374dcc40e1402726f4a2d74ca021f18cf6e
Sean Zellmer <sean@lejeunerenard.com>
2012-09-15 19:16:07 -0700

    Widgetize me captain

--- jquery.listable.js
+++ jquery.listable.js
@@ -1,48 +1,81 @@
+/*!
+ * jQuery Listable Plugin
+ * Author: @lejeunerenard
+ * Licensed under the LGPL license
+ */
+
+
 ;(function ( $, window, document, undefined ) {
+			
 	var update = false; // Whether the current form is an update or not
-	var counter = 1;
 	var place = 0;
 	var tooltip_enabled = false; // Whether the tooltips are turned on or not
 	var no_focus = true;
 
+	$.widget( "ui.listable", $.ui.mouse, {
+		// Options to be used as defaults
+		options: {
+			'edit'			: true,
+			'delete'		: true,
+			'add'			: true,
+			'keyboard_shortcuts'	: false,
+			'add_image'		: '/images/add_field.png',
+			'edit_image'		: '/images/edit_button.png',
+			'delete_image'		: '/images/delete_field.png',
+			'field_dividers_enabled': true,
+			'add_after'		: true,
+			'after_save'		: null,
+			'before_delete'		: null,
+			'after_delete'		: null,
+			'image_dragging'	: false,
+			'types'			: []
+		},
 
+		_create: function() {
+			if ($.fn.listable.counter === undefined) {
+				$.fn.listable.counter = 0;
+			}
+			$.fn.listable.counter += $('li.form_field').length + 1;
 
-	var methods = {
-		init: function( options ) {
-			counter = $(this).find('li.form_field').length + 1;
+			var settings = this.options;
 
-			$.fn.listable.settings = $.extend($.fn.listable.settings, { 'variable_vault'	: this.parent('form') }, options);	// Add a default for variable valut. This is not added at the bottom of this document because this is not defined there.
-			var settings = $.fn.listable.settings;	// Alias setting settings as an alias for $.fn.listable.settings
+			// Set variable vault
+			if ( !settings.variable_vault ) {
+				settings.variable_vault = this.element.parent('form');
+			}
 
+			if ( settings.image_dragging ) {
+				this.element.find('img').live('dragstart', function(event) { event.preventDefault(); });
+			}
 
 			if (settings.keyboard_shortcuts) {	// If the setting for keyboard_shortcuts is true then add the event listners for keyboard shortcuts. These event listeners are not full abstracted and the default for keyboard_shortcuts should be false.
 				$(window).keypress(function(event){
 				if (event.which == 106 && no_focus) { // j is pressed
-				 if ($.fn.listable.current_divider && $.fn.listable.current_divider.parent().length) {
-				    previous_divider = $.fn.listable.current_divider;
-				    if ($.fn.listable.current_divider.next().next('.field_divider').length) {
-				       $.fn.listable.current_divider = $.fn.listable.current_divider.next().next('.field_divider');
-				       $.fn.listable.current_divider.attr('hil','highlighted');
+				 if (this.current_divider && this.current_divider.parent().length) {
+				    previous_divider = this.current_divider;
+				    if (this.current_divider.next().next('.field_divider').length) {
+				       this.current_divider = this.current_divider.next().next('.field_divider');
+				       this.current_divider.attr('hil','highlighted');
 				       previous_divider.attr('hil','');
 				    }
 				 } else {
 				    $('.field_divider:first').attr('hil','highlighted');
-				    $.fn.listable.current_divider = $('.field_divider:first');
+				    this.current_divider = $('.field_divider:first');
 				 }
 				} else if (event.which == 107 && no_focus) { // k is pressed
-				 if ($.fn.listable.current_divider && $.fn.listable.current_divider.parent().length) {
-				    previous_divider = $.fn.listable.current_divider;
-				    if ($.fn.listable.current_divider.prev().prev('.field_divider').length) {
-				       $.fn.listable.current_divider = $.fn.listable.current_divider.prev().prev('.field_divider');
-				       $.fn.listable.current_divider.attr('hil','highlighted');
+				 if (this.current_divider && this.current_divider.parent().length) {
+				    previous_divider = this.current_divider;
+				    if (this.current_divider.prev().prev('.field_divider').length) {
+				       this.current_divider = this.current_divider.prev().prev('.field_divider');
+				       this.current_divider.attr('hil','highlighted');
 				       previous_divider.attr('hil','');
 				    }
 				 } else {
 				    $('.field_divider:last').attr('hil','highlighted');
-				    $.fn.listable.current_divider = $('.field_divider:last');
+				    this.current_divider = $('.field_divider:last');
 				 }
 				} else if (event.which == 97 && no_focus) { // a is pressed
-				 if ($.fn.listable.current_divider && $.fn.listable.current_divider.parent().length) {
+				 if (this.current_divider && this.current_divider.parent().length) {
 				    $('#fancy_inline form').hide();
 				    $('#textfield_form').show();
 				    $.fancybox({
@@ -56,7 +89,7 @@
 				    });
 				 } 
 				} else if (event.which == 115 && no_focus) { // s is pressed
-				 if ($.fn.listable.current_divider && $.fn.listable.current_divider.parent().length) {
+				 if (this.current_divider && this.current_divider.parent().length) {
 				    $('#fancy_inline form').hide();
 				    $('#textarea_form').show();
 				    $.fancybox({
@@ -70,7 +103,7 @@
 				    });
 				 } 
 				} else if (event.which == 100 && no_focus) { // d is pressed
-				 if ($.fn.listable.current_divider && $.fn.listable.current_divider.parent().length) {
+				 if (this.current_divider && this.current_divider.parent().length) {
 				    $('#fancy_inline form').hide();
 				    $('#checkbox_form').show();
 				    $.fancybox({
@@ -85,7 +118,7 @@
 				    });
 				 } 
 				} else if (event.which == 102 && no_focus) { // f is pressed
-				 if ($.fn.listable.current_divider && $.fn.listable.current_divider.parent().length) {
+				 if (this.current_divider && this.current_divider.parent().length) {
 				    $('#fancy_inline form').hide();
 				    $('#radio_form').show();
 				    $.fancybox({
@@ -100,7 +133,7 @@
 				    });
 				 } 
 				} else if (event.which == 103 && no_focus) { // g is pressed
-				 if ($.fn.listable.current_divider && $.fn.listable.current_divider.parent().length) {
+				 if (this.current_divider && this.current_divider.parent().length) {
 				    $('#fancy_inline form').hide();
 				    $('#select_form').show();
 				    $.fancybox({
@@ -115,7 +148,7 @@
 				    });
 				 } 
 				} else if (event.which == 122 && no_focus) { // g is pressed
-				 if ($.fn.listable.current_divider && $.fn.listable.current_divider.parent().length) {
+				 if (this.current_divider && this.current_divider.parent().length) {
 				    $('#fancy_inline form').hide();
 				    $('#email_form').show();
 				    $.fancybox({
@@ -130,16 +163,16 @@
 				    });
 				 } 
 				} else if (event.which == 101 && no_focus && settings.delete) { // e is pressed
-				 if ($.fn.listable.current_divider && $.fn.listable.current_divider.prev().prev('.field_divider').length) {
-				    new_divider = $.fn.listable.current_divider.prev().prev('.field_divider');
-				    $('.field_'+$.fn.listable.current_divider.prev().children('.delete_field').attr('class').replace(/delete_field field_/,'')).remove();
-				    $.fn.listable.current_divider.prev().remove();
-				    $.fn.listable.current_divider.remove();
-				    $.fn.listable.current_divider = new_divider;
-				    $.fn.listable.current_divider.attr('hil','highlighted');
+				 if (this.current_divider && this.current_divider.prev().prev('.field_divider').length) {
+				    new_divider = this.current_divider.prev().prev('.field_divider');
+				    $('.field_'+this.current_divider.prev().children('.delete_field').attr('class').replace(/delete_field field_/,'')).remove();
+				    this.current_divider.prev().remove();
+				    this.current_divider.remove();
+				    this.current_divider = new_divider;
+				    this.current_divider.attr('hil','highlighted');
 				 } 
 				} else if (event.which == 120 && no_focus) { // x is pressed
-				 if ($.fn.listable.current_divider && $.fn.listable.current_divider.parent().length) {
+				 if (this.current_divider && this.current_divider.parent().length) {
 				    $('#fancy_inline form').hide();
 				    $('#tinymce_form').show();
 				    $.fancybox({
@@ -154,32 +187,32 @@
 				    });
 				 } 
 				} else if (event.which == 99 && no_focus) { // x is pressed
-				 if ($.fn.listable.current_divider && $.fn.listable.current_divider.parent().length) {
+				 if (this.current_divider && this.current_divider.parent().length) {
 				var checked = 1;
-				$.fn.listable.current_divider.after('\
-				<li class="form_field field_'+counter+'">\
-				<label class="field_'+counter+'">Opinion (Captcha)</label>\
-				<a class="delete_field field_'+counter+'" href="#"><img src="'+settings.delete_image+'" alt="delete field" /></a>\
+				this.current_divider.after('\
+				<li class="form_field field_'+$.fn.listable.counter+'">\
+				<label class="field_'+$.fn.listable.counter+'">Opinion (Captcha)</label>\
+				<a class="delete_field field_'+$.fn.listable.counter+'" href="#"><img src="'+settings.delete_image+'" alt="delete field" /></a>\
 				</li>\
-				<li class="field_divider field_'+counter+'">\
+				<li class="field_divider field_'+$.fn.listable.counter+'">\
 				  <img src="'+settings.add_image+'" alt="add field" />\
 				</li>');
-				$(settings.variable_vault).append('<input type="hidden" name="field_label[]" value="opinion" class="field_'+counter+'" >\
-				<input type="hidden" name="field_type[]" value="captcha" class="field_'+counter+'" >\
-				<input type="hidden" name="field_order[]" value="0" class="field_'+counter+'" >\
-				<input type="hidden" name="field_length[]" value="16" class="field_'+counter+'" >\
-				<input type="hidden" name="field_mandatory[]" value="0" class="field_'+counter+'" >\
-				<input type="hidden" name="field_static_label[]" value="" class="field_'+counter+'" >\
-				<input type="hidden" name="field_inline[]" value="" class="field_'+counter+'" >\
-				<input type="hidden" name="field_validation[]" value="captcha" class="field_'+counter+'" >\
-				<input type="hidden" name="field_options[]" value="" class="field_'+counter+'" >\
+				$(settings.variable_vault).append('<input type="hidden" name="field_label[]" value="opinion" class="field_'+$.fn.listable.counter+'" >\
+				<input type="hidden" name="field_type[]" value="captcha" class="field_'+$.fn.listable.counter+'" >\
+				<input type="hidden" name="field_order[]" value="0" class="field_'+$.fn.listable.counter+'" >\
+				<input type="hidden" name="field_length[]" value="16" class="field_'+$.fn.listable.counter+'" >\
+				<input type="hidden" name="field_mandatory[]" value="0" class="field_'+$.fn.listable.counter+'" >\
+				<input type="hidden" name="field_static_label[]" value="" class="field_'+$.fn.listable.counter+'" >\
+				<input type="hidden" name="field_inline[]" value="" class="field_'+$.fn.listable.counter+'" >\
+				<input type="hidden" name="field_validation[]" value="captcha" class="field_'+$.fn.listable.counter+'" >\
+				<input type="hidden" name="field_options[]" value="" class="field_'+$.fn.listable.counter+'" >\
 				');
-				counter++;
+				$.fn.listable.counter++;
 				$('.controls').hide('fast');
 				no_focus = true;
 				 } 
 				} else if (event.which == 118 && no_focus) { // x is pressed
-				 if ($.fn.listable.current_divider && $.fn.listable.current_divider.parent().length) {
+				 if (this.current_divider && this.current_divider.parent().length) {
 				    $('#fancy_inline form').hide();
 				    $('#file_form').show();
 				    $.fancybox({
@@ -194,7 +227,7 @@
 				    });
 				 } 
 				} else if (event.which == 98 && no_focus) { // b is pressed
-				 if ($.fn.listable.current_divider && $.fn.listable.current_divider.parent().length) {
+				 if (this.current_divider && this.current_divider.parent().length) {
 				    $('#fancy_inline form').hide();
 				    $('#datepicker_form').show();
 				    $.fancybox({
@@ -209,7 +242,7 @@
 				    });
 				 } 
 				} else if (event.which == 119 && no_focus) { // w is pressed
-				 if ($.fn.listable.current_divider && $.fn.listable.current_divider.parent().length) {
+				 if (this.current_divider && this.current_divider.parent().length) {
 				    $('#fancy_inline form').hide();
 				    $('#session_variable_form').show();
 				    $.fancybox({
@@ -246,11 +279,11 @@
 			if (settings.field_dividers_enabled) {
 				$('.field_divider').live('mouseover mouseout', function(event) {	// Fade effect for hovering over current divider
 					if (event.type == 'mouseover') {
-						$(this).stop();
-						$(this).fadeTo('slow', 0.5);
+						this.element.stop();
+						this.element.fadeTo('slow', 0.5);
 					} else {
-						$(this).stop();
-						$(this).fadeTo('slow', 1);
+						this.element.stop();
+						this.element.fadeTo('slow', 1);
 					}
 				});
 
@@ -259,11 +292,11 @@
 					$('.controls').show('fast');
 					$('.controls').css('left',$(this).offset().left+parseInt($(this).css('width'))+25);
 					$('.controls').css('top',$(this).offset().top+parseInt($(this).css('height'))/2-25);
-					if ($.fn.listable.current_divider) {
-					 $.fn.listable.current_divider.attr('hil','');
+					if (this.current_divider) {
+					 this.current_divider.attr('hil','');
 					}
-					$.fn.listable.current_divider = $(this);
-					$.fn.listable.current_divider.attr('hil','highlighted');
+					this.current_divider = $(this);
+					this.current_divider.attr('hil','highlighted');
 				});
 			}
 			$('#controls_close').click(function(event){
@@ -271,7 +304,7 @@
 				$('.controls').hide('fast');
 			});
 			if (settings.delete) {	// If the delete setting is set to true then enable the delete button
-				$('.delete_field').live('click', function(event){
+				this.element.find('.delete_field').live('click', function(event){
 					if (typeof settings.before_delete == 'function') {
 						settings.before_delete(this);
 					}
@@ -286,7 +319,7 @@
 			}
 			if (settings.edit) {	// If the edit setting is set to true then enable the edit button
 
-				$('.edit_field').live('click', function(event){
+				this.element.find('.edit_field').live('click', function(event){
 					edit_link = $(this);
 					update = $(this).attr('class').replace(/edit_field /,'');
 
@@ -326,7 +359,7 @@
 					event.preventDefault();
 				});
 			}
-			$(this).sortable({	// Enable the items to be sortable
+			this.element.sortable({	// Enable the items to be sortable
 				items: '.form_field',
 				cancel: '.field_divider',
 				placeholder: 'place_holder',
@@ -348,8 +381,24 @@
 			
 			return this;
 		},
+
+		destroy: function() {
+			$.Widget.prototype.destroy.call(this);
+		},
+
+		_setOption: function( key, value ) {
+			switch( key ) {
+				default:
+					this.options[ key ] = value;
+					break;
+			}
+
+			$.Widget.prototype._setOption.apply( this, arguments );
+		},
+
+
 		save: function( itemType ) {
-			var settings = $.fn.listable.settings;
+			var settings = this.options;
 			if (update) { // Check to see if the user is updating an item or creating a new one
 				var vars = {};
 				$.each(itemType.variables, function(index, value) {	// Iterate through field variables and colect values. These values are stored in vars under the name of the variable
@@ -379,6 +428,7 @@
 				$('li.form_field.'+update).html(html_text);
 			} else {
 				var vars = {};
+				var tmp_counter = $.fn.listable.counter;
 				$.each(itemType.variables, function(index, value) {	// Iterate through field variables and colect values. These values are stored in vars under the name of the variable
 
 					if ($('#'+itemType.prefix+'_'+value).attr('type') == 'checkbox') {	// Check to see if its a checkbox type input since checkboxes need to be checked differently than normal inputs
@@ -391,37 +441,37 @@
 					} else {
 						vars[value] = $('#'+itemType.prefix+'_'+value).val();
 					}
-		       			$(settings.variable_vault).append('<input type="hidden" name="'+value+'[]" value="'+vars[value]+'" class="field_'+counter+'" >');	// Add the hidden input element to the variable vault
+		       			$(settings.variable_vault).append('<input type="hidden" name="'+value+'[]" value="'+vars[value]+'" class="field_'+tmp_counter+'" >');	// Add the hidden input element to the variable vault
 				});
 					//	Now add standard variables like order and type
-		       			$(settings.variable_vault).append('<input type="hidden" name="order[]" value="0" class="field_'+counter+'" >\
-					<input type="hidden" name="type[]" value="'+itemType.type+'" class="field_'+counter+'" >');
+		       			$(settings.variable_vault).append('<input type="hidden" name="order[]" value="0" class="field_'+$.fn.listable.counter+'" >\
+					<input type="hidden" name="type[]" value="'+itemType.type+'" class="field_'+$.fn.listable.counter+'" >');
 				// Calculate then add appropriate html for the new item
 				var append_text = '\
-		       <li class="form_field field_'+counter+'">\
+		       <li class="form_field field_'+$.fn.listable.counter+'">\
 				'+itemType.display(vars);
 				if (settings.edit) {
 					append_text += '\
-			       <a class="edit_field field_'+counter+'" href="#"><img src="'+settings.edit_image+'" alt="edit field" /></a>';
+			       <a class="edit_field field_'+$.fn.listable.counter+'" href="#"><img src="'+settings.edit_image+'" alt="edit field" /></a>';
 				}
 				if (settings.delete) {
 					append_text += '\
-			       <a class="delete_field field_'+counter+'" href="#"><img src="'+settings.delete_image+'" alt="delete field" /></a>';
+			       <a class="delete_field field_'+$.fn.listable.counter+'" href="#"><img src="'+settings.delete_image+'" alt="delete field" /></a>';
 				}
 				append_text += '\
 		       </li>';
 				if (settings.field_dividers_enabled) {
 					append_text += '\
-		       <li class="field_divider field_'+counter+'">\
+		       <li class="field_divider field_'+$.fn.listable.counter+'">\
 			  <img src="'+settings.add_image+'" alt="add field" />\
 		       </li>';
 				}
 				if (settings.add_after) {
-					$.fn.listable.current_divider.after(append_text);
+					this.current_divider.after(append_text);
 				} else {
-					$.fn.listable.current_divider.before(append_text);
+					this.current_divider.before(append_text);
 				}
-		       counter++;
+		       $.fn.listable.counter++;
 			}
 			if (typeof settings.after_save == 'function') {
 				settings.after_save(itemType);
@@ -429,35 +479,7 @@
 			
 			return this;
 		}
-	};
-
-	$.fn.listable = function( method ) {
-
-	// Redirect to appropriate method and give it the appropriate arguments
+	});
 
 
-		if ( methods[method] ) {
-			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-		} else if ( typeof method === 'object' || !method ) {
-			return methods.init.apply( this, arguments );
-		} else {
-			$.error( 'Method ' + method + ' does not exist on jQuery.listable' );
-		}
-
-	};
-	$.fn.listable.settings = $.extend({	// Set default settings
-		'edit'			: true,
-		'delete'		: true,
-		'add'			: true,
-		'keyboard_shortcuts'	: false,
-		'add_image'		: '/images/add_field.png',
-		'edit_image'		: '/images/edit_button.png',
-		'delete_image'		: '/images/delete_field.png',
-		'field_dividers_enabled': true,
-		'add_after'		: true,
-		'after_save'		: null,
-		'before_delete'		: null,
-		'after_delete'		: null,
-		'types'			: []
-	}, $.fn.listable.settings);
 })( jQuery, window, document );	// End of encapsulation
