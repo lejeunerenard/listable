210c0839b4ebaa0ed9b09fc76569390d6e8b327f
Sean Zellmer <sean@lejeunerenard.com>
2013-10-25 11:09:26 -0500

    Added jshint module

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,16 @@
 //Wrapper function with one parameter
 module.exports = function(grunt) {
-
-  // What to do by default. In this case, nothing.
-  grunt.registerTask('default', []);
+   grunt.initConfig({
+      jshint: {
+         options: {
+            eqeqeq: true,
+            trailing: true
+         },
+         target: {
+            src: ['src/**/*.js']
+         }
+      }
+   });
+   grunt.loadNpmTasks('grunt-contrib-jshint');
+   grunt.registerTask('default', ['jshint']);
 };
